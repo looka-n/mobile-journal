64cf942a5e25416ad87f245fa8c9fec9ec2f4c67
Lucas Nguyen <nguyenlucasv@gmail.com>
2024-03-18 22:20:08 -0700

    Restart 3.18.2024

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { View } from 'react-native'
-import CardViewEditable from './components/CardViewEditable';
-import CardViewFull from './components/CardViewFull';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-
-const Stack = createStackNavigator();
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="CardViewEditable" component={CardViewEditable} />
-        <Stack.Screen name="CardViewFull" component={CardViewFull} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+    <HomeScreen />
+  ) }
 
 export default App;
---
+++ components/home/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native'
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header
