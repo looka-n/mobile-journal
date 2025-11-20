2a8ac062ea312c2ff9f7aca8181951898f69b885
Lucas Nguyen <nguyenlucasv@gmail.com>
2025-11-19 20:03:34 -0800

    separate firebase config

--- firebase/FirebaseConfig.js
+++ firebase/FirebaseConfig.js
@@ -3,21 +3,12 @@ import { getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { firebaseKeys } from "./firebaseKeys";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB3tRChmnV82Fb8wCxwQF0KDzNGAXN013A",
-  authDomain: "mobile-journal-30c6a.firebaseapp.com",
-  projectId: "mobile-journal-30c6a",
-  storageBucket: "mobile-journal-30c6a.firebasestorage.app",
-  messagingSenderId: "180278538688",
-  appId: "1:180278538688:web:448b1b2445e4e79cecd825"
-};
-
 // Initialize Firebase
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseKeys);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
